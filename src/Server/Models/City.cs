38cc78bf2a6be46e16d1d511095c0c6a87a10978
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-11 15:59:45 +0300

    Models has been added

---
+++ src/Server/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Houses.Common.GlobalConstants.ValidationConstants.City;
+
+namespace Houses.Infrastructure.Data.Entities
+{
+    public class City
+    {
+
+        [Key]
+        public string Id { get; set; } = null!;
+
+        [MaxLength(CityMaxName)]
+        public string Name { get; set; } = null!;
+    }
+}
