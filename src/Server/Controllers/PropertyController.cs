77d6d55ed826e70bef5223e9e9a6662494686e3a
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-13 13:46:13 +0300

    Controllers/PropertyController.cs has been added

---
+++ src/Server/Controllers/PropertyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Data.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PropertyController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PropertyController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPropertiesAsync()
+        {
+            var properties = await _context.Properties.ToListAsync();
+
+            return Ok(properties);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> AddPropertiesAsync([FromBody] Property propertyRequest)
+        {
+            propertyRequest.Id = Guid.NewGuid().ToString();
+
+            await _context.Properties.AddAsync(propertyRequest);
+            await _context.SaveChangesAsync();
+
+            return Ok(propertyRequest);
+        }
+    }
+}
