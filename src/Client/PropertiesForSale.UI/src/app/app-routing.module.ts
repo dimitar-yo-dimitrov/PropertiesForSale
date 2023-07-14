918f206caf6a387aa7b82b46b41c4b28991f4037
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-14 16:05:57 +0300

    properties-list component has been added

--- src/Client/PropertiesForSale.UI/src/app/app-routing.module.ts
+++ src/Client/PropertiesForSale.UI/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertiesListComponent } from './components/properties/properties-list/properties-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PropertiesListComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/Client/PropertiesForSale.UI/src/app/app.module.ts
+++ src/Client/PropertiesForSale.UI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertiesListComponent } from './components/properties/properties-list/properties-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertiesListComponent
   ],
   imports: [
     BrowserModule,
