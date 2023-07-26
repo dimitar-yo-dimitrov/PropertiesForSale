2b218b83708ecd05ff3afc866bd7c06560cf6696
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-26 16:09:32 +0300

    environments and get method in api.service has been added

---
+++ src/Client/PropertiesForSale.UI/src/app/core/services/api.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http
+      .get(`${environment.apiUrl}${path}`, { params })
+      .pipe(catchError(this.formatErrors));
+  }
+  formatErrors(error: any): Observable<never> {
+    return throwError(() => new Error('Error from api.service'));
+  }
+}
