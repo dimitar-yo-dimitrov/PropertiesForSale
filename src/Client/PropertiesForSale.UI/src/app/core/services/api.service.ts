87a2e006ba4d821bf1c02a935aaff3d5e9ab87b0
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-26 16:14:28 +0300

    put, post, delete methods is added

--- src/Client/PropertiesForSale.UI/src/app/core/services/api.service.ts
+++ src/Client/PropertiesForSale.UI/src/app/core/services/api.service.ts
@@ -14,6 +14,25 @@ export class ApiService {
       .get(`${environment.apiUrl}${path}`, { params })
       .pipe(catchError(this.formatErrors));
   }
+
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .put(`${environment.apiUrl}${path}`, body)
+      .pipe(catchError(this.formatErrors));
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http
+      .post(`${environment.apiUrl}${path}`, body)
+      .pipe(catchError(this.formatErrors));
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http
+      .delete(`${environment.apiUrl}${path}`)
+      .pipe(catchError(this.formatErrors));
+  }
+
   formatErrors(error: any): Observable<never> {
     return throwError(() => new Error('Error from api.service'));
   }
