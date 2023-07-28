e9281d083458f45ddb815fe624adba3c2865161d
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-07-28 15:40:08 +0300

    WIP for create

--- src/Client/PropertiesForSale.UI/src/app/components/properties/property-routing.module.ts
+++ src/Client/PropertiesForSale.UI/src/app/components/properties/property-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { PropertiesListComponent } from './properties-list/properties-list.component';
+import { CreateComponent } from './create/create.component';
 
-const routes: Routes = [{ path: '', component: PropertiesListComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: PropertiesListComponent },
+  { path: '**', pathMatch: 'full', component: CreateComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/Client/PropertiesForSale.UI/src/app/core/core.module.ts
+++ src/Client/PropertiesForSale.UI/src/app/core/core.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { PropertiesListComponent } from '../components/properties/properties-list/properties-list.component';
-import { CreateComponent } from '../components/properties/create/create.component';
 
 @NgModule({
   declarations: [],
