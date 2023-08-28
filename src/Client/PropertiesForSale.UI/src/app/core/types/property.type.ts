b61288423533d96c088f1d845e1830330e9c7cd9
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-08-28 15:31:11 +0300

    the structure of the architecture in the application has been changed

--- src/Client/PropertiesForSale.UI/src/app/core/types/property.type.ts
+++
@@ -1,9 +0,0 @@
-export interface IProperty {
-    id: string;
-    title: string;
-    price: number;
-    address: string;
-    description: string;
-    squareMeters: number;
-    imageUrl: string;
-}
